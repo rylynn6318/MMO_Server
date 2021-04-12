cfe15d4813532073654f6e4420b8d132e012902f
donghyeon <rylynn6318@kumoh.ac.kr>
2021-04-12 15:40:57 +0900

    add Listener

---
+++ ServerCore/Listener.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    class Listener
+    {
+        Socket listenSocket;
+        Action<Socket> onAcceptHander;
+
+        public void Init(IPEndPoint endPoint, Action<Socket> AcceptHander)
+        {
+            listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            onAcceptHander += AcceptHander;
+            
+            listenSocket.Bind(endPoint);
+
+            listenSocket.Listen(10);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
+        }
+
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            //기존 내용 제거
+            args.AcceptSocket = null;
+
+            bool pending = listenSocket.AcceptAsync(args);
+            if (pending == false)
+                OnAcceptCompleted(null, args);
+        }
+        void OnAcceptCompleted(Object sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                onAcceptHander.Invoke(args.AcceptSocket);
+            }
+            else
+            {
+                Console.WriteLine(args.SocketError.ToString());
+            }
+
+            //다음을 위해 등록
+            RegisterAccept(args);
+        }
+        public Socket Accept()
+        {
+            
+            return listenSocket.Accept();
+        }
+    }
+}
--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace ServerCore
 {
     class Program
     {
+        static Listener listener = new Listener();
+
+        static void OnAcceptHander(Socket clientSocket)
+        {
+            try
+            {
+                //받음
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = clientSocket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Client] {recvData}");
+
+                //보냄
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server!");
+                clientSocket.Send(sendBuff);
+
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //DNS
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+            
+            listener.Init(endPoint, OnAcceptHander);
+            while (true)
+            {
+
+            }
         }
     }
 }
